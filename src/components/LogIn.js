62c0daa14d6a32c071f1f207d5ddc4702796631f
Pete Alberico <palberico@yahoo.com>
2018-09-23 15:47:32 -0600

    Added gradient and aarows

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -3,7 +3,8 @@ import { Container, Card, Image, Button, Icon } from 'semantic-ui-react';
 import Stage from '../images/stage.jpg';
 import Logo from '../images/logo.svg';
 import Forward from '../images/forward.svg';
-import firebase, { auth, provider } from '../firebase.js';
+import Down from '../images/down.svg';
+import Firebase, { auth, provider } from '../firebase.js';
 
 class LogIn extends Component {
   constructor() {
@@ -48,13 +49,14 @@ class LogIn extends Component {
   render() {
     return (
       <div style={styles.app}>
-        <div style={styles.forward} />
         <div style={styles.card}>
           <div style={styles.logoContainer}>
-            <div style={styles.logo} />
+            <div style={styles.down} />
+              <div style={styles.logo} />
           </div>
           <div style={styles.catchPhrase}>
             live. local. music.
+            <div style={styles.forward} />
           </div>
           <div style={styles.buttonContainer}>
             <div style={styles.googleBtn} onClick={this.login}>
@@ -80,31 +82,40 @@ const styles = {
 
   forward: {
     height: '50px',
-    width: '50px',  
-    marginLeft: '420px', 
-    marginTop: '80px', 
+    width: '50px',
+    float: 'right',  
     backgroundImage: `url(${Forward})`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'contain',
     animation: 'pulse 3s infinite'
   },
 
+  down: {
+    height: '50px',
+    width: '50px',  
+    backgroundImage: `url(${Down})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'contain',
+    animation: 'pulse 3s infinite',
+    display: 'flex',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: '0px',
+  },
+
+
   card: {
     position: 'absolute',
     left: '0px',
-    marginTop: '50px',
-    height: '500px',
-    width: '800px',
-    backgroundColor: 'rgba(14, 15, 40, 0.80)',
-    borderStyle: 'solid',
-    borderImage: 'linear-gradient(90deg, #0E0F28, #217AD8)',
-    borderImageSlice: '1',
-    borderWidth: '1px'
-  
+    top: '0px',
+    height: '100%',
+    width: '100%',
+    backgroundImage: 'linear-gradient(to top right, rgba(172, 29, 132, 0.9), rgba(97, 18, 106, 0.5), rgba(73, 15, 121, 0.5), rgba(28, 31, 50, 0.9))',
+    backgroundSize: 'cover',
   },
 
   logoContainer: {
-    marginTop: '25px',
+    marginTop: '125px',
     display: 'flex',
     justifyContent: 'center',
     width: '100%',
@@ -116,6 +127,8 @@ const styles = {
     backgroundImage: `url(${Logo})`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'contain',
+    display: 'flex',
+    justifyContent: 'center',
   },
 
   catchPhrase: {
@@ -141,7 +154,7 @@ const styles = {
     height: '50px',
     lineHeight: '45px',
     cursor: 'pointer',
-  }
+  },
 }
 
 export default LogIn;
